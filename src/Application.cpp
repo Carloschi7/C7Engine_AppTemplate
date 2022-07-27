e3b1d71407c000b4cbe2255693c08f6a0c369021
carlo negrini <carloschi23@libero.it>
2022-07-28 00:04:02 +0200

    Added new shaders

--- src/Application.cpp
+++ src/Application.cpp
@@ -19,39 +19,85 @@ void Application::OnUserCreate()
 
 void Application::OnUserRun()
 {
-	//Cube's 8 vertices
-	float verts[]{ -2.5f, -2.5f,  0.0f,  1.0f,  0.0f,  0.0f,
-				    2.5f, -2.5f,  0.0f,  0.0f,  1.0f,  0.0f,
-				    2.5f,  2.5f,  0.0f,  0.0f,  0.0f,  1.0f,
-				   -2.5f,  2.5f,  0.0f,  1.0f,  1.0f,  0.0f,
-				   -2.5f, -2.5f, -5.0f,  1.0f,  0.0f,  0.0f,
-					2.5f, -2.5f, -5.0f,  0.0f,  1.0f,  0.0f,
-					2.5f,  2.5f, -5.0f,  0.0f,  0.0f,  1.0f,
-				   -2.5f,  2.5f, -5.0f,  1.0f,  1.0f,  0.0f };
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	//Indexed as a drawing of multiple triangles
-	uint32_t indices[]{ 0,1,2,2,3,0, // FRONT
-						1,5,6,6,2,1, // RIGHT
-						4,5,6,6,7,4, // BACK
-						4,0,3,3,7,4, // LEFT
-						3,2,6,6,7,3, // TOP
-						0,1,5,5,4,0}; // BOTTOM
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
 
 	Layout lyt;
-	lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0 });
-	lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 3 * sizeof(float)});
+	lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0 });
+	lyt.PushAttribute({ 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 3 * sizeof(float)});
 
-	m_Camera.SetVectors(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	m_Camera.SetVectors(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, -1.0f));
 	float fAspectRatio = float(m_Window.Width()) / float(m_Window.Height());
-	m_Camera.SetPerspectiveValues(glm::radians(45.0f), fAspectRatio, 0.1f, 100.0f);
+	m_Camera.SetPerspectiveValues(glm::radians(45.0f), fAspectRatio, 0.1f, 1000.0f);
 	m_Camera.SetKeyboardFunction(KeyboardForCameraFun);
 	m_Camera.SetMouseFunction(MouseForCameraFun);
 
 
-	VertexManager manager(verts, sizeof(verts), indices, sizeof(indices), lyt);
-	Entity obj(manager);
+	VertexManager manager(vertices, sizeof(vertices), lyt);
+	
 
-	Shader shd("assets/basic.shader");
+	Shader shd("assets/shaders/basic_texture.shader");
+    Shader cubeshd("assets/shaders/basic_cubemap.shader");
+    Texture tex("assets/images/saul_goodman.png", true);
+    shd.Uniform1i(0, "texture1");
+
+    std::vector<std::string> vec({ "assets/images/skybox/right.jpg",
+                                    "assets/images/skybox/left.jpg",
+                                    "assets/images/skybox/bottom.jpg",
+                                    "assets/images/skybox/top.jpg",
+                                    "assets/images/skybox/front.jpg",
+                                    "assets/images/skybox/back.jpg"
+        });
+    CubeMap cube(vec, 500.0f);
+    cube.BindTexture(1);
+    cubeshd.Uniform1i(1, "skybox");
+    
+
+    Entity obj(manager);
+    obj.Scale(20.0f);
+    Entity cubeobj(cube.GetVertexManager());
+
 
 	m_Window.SetVsync(true);
 	glEnable(GL_DEPTH_TEST);
@@ -61,10 +107,16 @@ void Application::OnUserRun()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		m_Camera.ProcessInput(m_Window, 1.0);
-		shd.UniformMat4f(m_Camera.GetViewMatrix(), "view");
-		shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+		
+        shd.UniformMat4f(m_Camera.GetViewMatrix(), "view");
+        shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+        cubeshd.UniformMat4f(glm::mat4(glm::mat3(m_Camera.GetViewMatrix())), "view");
+        cubeshd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
 
+
+        cubeobj.Draw(cubeshd);
 		obj.Draw(shd);
 		m_Window.Update();
 	}
 }
+
