a79d56deff2e9a0c9f47fe3f07d0b8cba0e0b66a
carlo negrini <carloschi23@libero.it>
2022-07-30 19:21:08 +0200

    added new lighting shader

---
+++ assets/shaders/basic_lighting.shader
@@ -0,0 +1,43 @@
+#shader vertex
+#version 330 core
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 norm;
+layout(location = 2) in vec2 tex_coord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
+out vec3 Pos;
+out vec3 Norm;
+out vec2 TexCoord;
+
+void main()
+{
+	Pos = vec3(model * vec4(pos, 1.0f));
+	Norm = normalize(norm);
+	TexCoord = tex_coord;
+	gl_Position = proj * view * vec4(Pos, 1.0f);
+}
+
+#shader fragment
+#version 330 core
+
+uniform sampler2D texture1;
+uniform vec3 light_pos;
+
+in vec3 Pos;
+in vec3 Norm;
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+void main()
+{
+	vec3 PosToLight = light_pos - Pos;
+	float brightnessMultiplier = 0.01f;
+	float brightness = pow(length(PosToLight), 2) * brightnessMultiplier;
+	float diffuse = max(0.2f, dot(Norm, normalize(PosToLight)));
+	FragColor = (texture(texture1, TexCoord) * diffuse) / brightness;
+}
--- src/Application.cpp
+++ src/Application.cpp
@@ -20,53 +20,57 @@ void Application::OnUserCreate()
 void Application::OnUserRun()
 {
     float vertices[] = {
-        -10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-         10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-         10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-         10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-        -10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-        -10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-
-        -10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
-         10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   1.0f, 0.0f,
-         10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
-         10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
-        -10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   0.0f, 1.0f,
-        -10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
-
-        -10.0f,  10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-        -10.0f,  10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-        -10.0f, -10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-        -10.0f, -10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-        -10.0f, -10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-        -10.0f,  10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-
-         10.0f,  10.0f,  10.0f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-         10.0f,  10.0f, -10.0f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-         10.0f, -10.0f, -10.0f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-         10.0f, -10.0f, -10.0f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-         10.0f, -10.0f,  10.0f,    1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-         10.0f,  10.0f,  10.0f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-
-        -10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
-         10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,   1.0f, 1.0f,
-         10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-         10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-        -10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,   0.0f, 0.0f,
-        -10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
-
-        -10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-         10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-         10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-         10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-        -10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-        -10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
+                       
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+                       
+        -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+                       
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+                       
+        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
+                       
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
     };
 
     Layout lyt;
     lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0 });
     lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float) });
-    //lyt.PushAttribute({ 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float)});
+    lyt.PushAttribute({ 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float)});
+
+    Layout lyt2;
+    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0 });
+    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float) });
 
     m_Camera.SetVectors(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, -1.0f));
     float fAspectRatio = float(m_Window.Width()) / float(m_Window.Height());
@@ -75,25 +79,23 @@ void Application::OnUserRun()
     m_Camera.SetMouseFunction(MouseForCameraFun);
 
     VertexManager manager(vertices, sizeof(vertices), lyt);
+    VertexManager manager2(vertices, sizeof(vertices), lyt2);
 
-    Shader shd("assets/shaders/skybox_reflect.shader");
-    Shader cubeshd("assets/shaders/basic_cubemap.shader");
-
-    std::vector<std::string> vec({ "assets/images/skybox/right.jpg",
-                                    "assets/images/skybox/left.jpg",
-                                    "assets/images/skybox/bottom.jpg",
-                                    "assets/images/skybox/top.jpg",
-                                    "assets/images/skybox/front.jpg",
-                                    "assets/images/skybox/back.jpg"
-        });
-    CubeMap cube(vec, 500.0f);
-    cube.BindTexture(1);
-    shd.Uniform1i(1, "skybox");
-    cubeshd.Uniform1i(1, "skybox");
-
+    Shader shd("assets/shaders/basic_lighting.shader");
+    Shader shd2("assets/shaders/basic.shader");
+    shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+    shd2.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+    
+    
+    Texture tex("assets/images/parquet.jpg");
+    tex.Bind();
+    shd.Uniform1i(0, "texture1");
+    
 
     Entity obj(manager);
-    Entity cubeobj(cube.GetVertexManager());
+    Entity obj2(manager2);
+    obj.Scale(40.0f);
+    obj2.Translate(glm::vec3(25.0f, 0.0f, 0.0f));
 
 
     m_Window.SetVsync(true);
@@ -106,14 +108,13 @@ void Application::OnUserRun()
         m_Camera.ProcessInput(m_Window, 1.0);
 
         shd.UniformMat4f(m_Camera.GetViewMatrix(), "view");
-        shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
-        shd.UniformVec3f(m_Camera.GetPosition(), "camera_pos");
-        cubeshd.UniformMat4f(glm::mat4(glm::mat3(m_Camera.GetViewMatrix())), "view");
-        cubeshd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+        shd2.UniformMat4f(m_Camera.GetViewMatrix(), "view");
+        //Light pos
+        shd.UniformVec3f(m_Camera.GetPosition(), "light_pos");
+        
 
-
-        cubeobj.Draw(cubeshd);
         obj.Draw(shd);
+        obj2.Draw(shd2);
         m_Window.Update();
     }
 }
