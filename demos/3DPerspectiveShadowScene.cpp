4d311626855840f13cd316ba77d53a96e6386625
carlo <carloschi23@libero.it>
2022-10-01 22:30:41 +0200

    Added new demo

---
+++ demos/3DPerspectiveShadowScene.cpp
@@ -0,0 +1,153 @@
+#include "../application/Application.h"
+
+Application::Application(Window& window)
+        :m_Window(window)
+{
+
+}
+
+Application::~Application()
+{
+}
+
+void Application::OnUserCreate()
+{
+    m_Window.SetVsync(true);
+    m_Window.SetWndInCurrentContext();
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void Application::OnUserRun()
+{
+    float vertices[] = {
+            -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    };
+
+    Layout lyt2;
+    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0 });
+    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float)});
+    lyt2.PushAttribute({ 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float) });
+
+    m_Camera.SetVectors(glm::vec3(0.0f, 70.0f, 20.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+    float fAspectRatio = float(m_Window.Width()) / float(m_Window.Height());
+    m_Camera.SetPerspectiveValues(glm::radians(45.0f), fAspectRatio, 0.1f, 1000.0f);
+    m_Camera.SetKeyboardFunction(KeyboardForCameraFun);
+    m_Camera.SetMouseFunction(MouseForCameraFun);
+
+    VertexManager mgr(vertices, sizeof(vertices), lyt2);
+
+    Shader shd("assets/shaders/point_shadow.shader");
+    Shader shd_light("assets/shaders/basic.shader");
+    shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+    shd_light.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+
+    Texture tex("assets/images/parquet.jpg");
+
+    glm::vec3 lightPos(0.0f, 70.0f, 20.0f);
+
+    Entity obj(mgr);
+    Entity obj2(mgr);
+    Entity obj3(mgr);
+    Entity obj4(mgr);
+    Entity obj5(mgr);
+    obj.Scale(100.0f);
+    obj2.Translate(glm::vec3(3.0f, 55.0f, 0.0f));
+    obj2.Rotate(glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    obj2.Scale(2.0f);
+    obj3.Translate(glm::vec3(-3.0f, 55.0f, 0.0f));
+    obj3.Rotate(glm::radians(60.0f), glm::vec3(1.0f, 0.0f, 1.0f));
+    obj3.Scale(2.0f);
+    obj4.Translate(glm::vec3(-8.0f, 55.0f, 0.0f));
+    obj4.Rotate(glm::radians(70.0f), glm::vec3(0.0f, 1.0f, 1.0f));
+    obj4.Scale(2.0f);
+    obj5.Translate(lightPos);
+
+    //Create depth buffer
+    FrameBuffer depth_buffer(1000, 1000, FrameBufferType::DEPTH_ATTACHMENT);
+    Shader depth_shd("assets/shaders/depth.shader");
+
+    glm::mat4 lightProj = glm::perspective(glm::radians(45.0f), 1.0f, 1.0f, 100.0f);
+    glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 50.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    shd.UniformMat4f(lightProj * lightView, "light_space");
+    shd.UniformVec3f(lightPos, "light_pos");
+    depth_shd.UniformMat4f(lightProj * lightView, "space");
+
+    m_Window.SetVsync(true);
+    glEnable(GL_DEPTH_TEST);
+
+    while (!m_Window.ShouldClose())
+    {
+        //Drawing to depth texture
+        depth_buffer.Bind();
+        glViewport(0, 0, 1000, 1000);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        //obj.Draw(depth_shd);
+        obj2.Draw(depth_shd);
+        obj3.Draw(depth_shd);
+        obj4.Draw(depth_shd);
+
+        //Drawing to screen
+        FrameBuffer::BindDefault();
+        tex.Bind();
+        shd.Uniform1i(0, "diffuse_texture");
+        depth_buffer.BindFrameTexture(1);
+        shd.Uniform1i(1, "depth_texture");
+        glViewport(0, 0, 1920, 1080);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        m_Camera.ProcessInput(m_Window, 1.0);
+
+        //shd.UniformMat4f(lightProj, "proj");
+        //shd.UniformMat4f(lightView, "view");
+        shd.UniformVec3f(m_Camera.GetPosition(), "camera_pos");
+        shd.UniformMat4f(m_Camera.GetViewMatrix(), "view");
+        shd_light.UniformMat4f(m_Camera.GetViewMatrix(), "view");
+
+
+        obj.Draw(shd);
+        obj2.Draw(shd);
+        obj3.Draw(shd);
+        obj4.Draw(shd);
+        obj5.Draw(shd_light);
+        m_Window.Update();
+    }
+}
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,7 @@
 #include "../application/Application.h"
 
 Application::Application(Window& window)
-    :m_Window(window)
+        :m_Window(window)
 {
 
 }
@@ -20,134 +20,99 @@ void Application::OnUserCreate()
 void Application::OnUserRun()
 {
     float vertices[] = {
-        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, -1.0f,   0.0f, 0.0f,
-                       
-        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-                       
-        -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,    -1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-                       
-         0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-                       
-        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f, 0.0f,   0.0f, 1.0f,
-                       
-        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
+            -10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+            10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+            10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+            10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+            -10.0f,  10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+            -10.0f, -10.0f, -10.0f,    0.0f, 0.0f, 1.0f,
+
+            -10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+            10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+            10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+            10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+            -10.0f,  10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+            -10.0f, -10.0f,  10.0f,    0.0f, 0.0f, -1.0f,
+
+            -10.0f,  10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,
+            -10.0f,  10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,
+            -10.0f, -10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,
+            -10.0f, -10.0f, -10.0f,    -1.0f, 0.0f, 0.0f,
+            -10.0f, -10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,
+            -10.0f,  10.0f,  10.0f,    -1.0f, 0.0f, 0.0f,
+
+            10.0f,  10.0f,  10.0f,    1.0f, 0.0f, 0.0f,
+            10.0f,  10.0f, -10.0f,    1.0f, 0.0f, 0.0f,
+            10.0f, -10.0f, -10.0f,    1.0f, 0.0f, 0.0f,
+            10.0f, -10.0f, -10.0f,    1.0f, 0.0f, 0.0f,
+            10.0f, -10.0f,  10.0f,    1.0f, 0.0f, 0.0f,
+            10.0f,  10.0f,  10.0f,    1.0f, 0.0f, 0.0f,
+
+            -10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,
+            10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,
+            10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,
+            10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,
+            -10.0f, -10.0f,  10.0f,    0.0f, -1.0f, 0.0f,
+            -10.0f, -10.0f, -10.0f,    0.0f, -1.0f, 0.0f,
+
+            -10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,
+            10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,
+            10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,
+            10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,
+            -10.0f,  10.0f,  10.0f,    0.0f, 1.0f, 0.0f,
+            -10.0f,  10.0f, -10.0f,    0.0f, 1.0f, 0.0f,
     };
 
-    Layout lyt2;
-    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0 });
-    lyt2.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float)});
-    lyt2.PushAttribute({ 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float) });
+    Layout lyt;
+    lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0 });
+    lyt.PushAttribute({ 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 3 * sizeof(float) });
 
-    m_Camera.SetVectors(glm::vec3(0.0f, 70.0f, 20.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+    m_Camera.SetVectors(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, -1.0f));
     float fAspectRatio = float(m_Window.Width()) / float(m_Window.Height());
     m_Camera.SetPerspectiveValues(glm::radians(45.0f), fAspectRatio, 0.1f, 1000.0f);
     m_Camera.SetKeyboardFunction(KeyboardForCameraFun);
     m_Camera.SetMouseFunction(MouseForCameraFun);
 
-    VertexManager mgr(vertices, sizeof(vertices), lyt2);
+    VertexManager manager(vertices, sizeof(vertices), lyt);
 
-    Shader shd("assets/shaders/point_shadow.shader");
-    Shader shd_light("assets/shaders/basic.shader");
-    shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
-    shd_light.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+    Shader shd("assets/shaders/skybox_reflect.shader");
+    Shader cubeshd("assets/shaders/basic_cubemap.shader");
 
-    Texture tex("assets/images/parquet.jpg");
-    
-    glm::vec3 lightPos(0.0f, 70.0f, 20.0f);
+    std::vector<std::string> vec({
+        "assets/skyboxes/lake/left.jpg",
+        "assets/skyboxes/lake/right.jpg",
+        "assets/skyboxes/lake/bottom.jpg",
+        "assets/skyboxes/lake/top.jpg",
+        "assets/skyboxes/lake/back.jpg",
+        "assets/skyboxes/lake/front.jpg"
+    });
+    CubeMap cube(vec, 500.0f);
+    cube.BindTexture(1);
+    shd.Uniform1i(1, "skybox");
+    cubeshd.Uniform1i(1, "skybox");
 
-    Entity obj(mgr);
-    Entity obj2(mgr);
-    Entity obj3(mgr);
-    Entity obj4(mgr);
-    Entity obj5(mgr);
-    obj.Scale(100.0f);
-    obj2.Translate(glm::vec3(3.0f, 55.0f, 0.0f));
-    obj2.Rotate(glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    obj2.Scale(2.0f);
-    obj3.Translate(glm::vec3(-3.0f, 55.0f, 0.0f));
-    obj3.Rotate(glm::radians(60.0f), glm::vec3(1.0f, 0.0f, 1.0f));
-    obj3.Scale(2.0f);
-    obj4.Translate(glm::vec3(-8.0f, 55.0f, 0.0f));
-    obj4.Rotate(glm::radians(70.0f), glm::vec3(0.0f, 1.0f, 1.0f));
-    obj4.Scale(2.0f);
-    obj5.Translate(lightPos);
 
-    //Create depth buffer
-    FrameBuffer depth_buffer(1000, 1000, FrameBufferType::DEPTH_ATTACHMENT);
-    Shader depth_shd("assets/shaders/depth.shader");
+    Entity obj(manager);
+    Entity cubeobj(cube.GetVertexManager());
 
-    glm::mat4 lightProj = glm::perspective(glm::radians(45.0f), 1.0f, 1.0f, 100.0f);
-    glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 50.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    shd.UniformMat4f(lightProj * lightView, "light_space");
-    shd.UniformVec3f(lightPos, "light_pos");
-    depth_shd.UniformMat4f(lightProj * lightView, "space");
 
     m_Window.SetVsync(true);
     glEnable(GL_DEPTH_TEST);
 
     while (!m_Window.ShouldClose())
     {
-        //Drawing to depth texture
-        depth_buffer.Bind();
-        glViewport(0, 0, 1000, 1000);
-        glClear(GL_DEPTH_BUFFER_BIT);
-        //obj.Draw(depth_shd);
-        obj2.Draw(depth_shd);
-        obj3.Draw(depth_shd);
-        obj4.Draw(depth_shd);
-
-        //Drawing to screen
-        FrameBuffer::BindDefault();
-        tex.Bind();
-        shd.Uniform1i(0, "diffuse_texture");
-        depth_buffer.BindFrameTexture(1);
-        shd.Uniform1i(1, "depth_texture");
-        glViewport(0, 0, 1920, 1080);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         m_Camera.ProcessInput(m_Window, 1.0);
 
-        //shd.UniformMat4f(lightProj, "proj");
-        //shd.UniformMat4f(lightView, "view");
-        shd.UniformVec3f(m_Camera.GetPosition(), "camera_pos");
         shd.UniformMat4f(m_Camera.GetViewMatrix(), "view");
-        shd_light.UniformMat4f(m_Camera.GetViewMatrix(), "view");
-        
+        shd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
+        shd.UniformVec3f(m_Camera.GetPosition(), "camera_pos");
+        cubeshd.UniformMat4f(glm::mat4(glm::mat3(m_Camera.GetViewMatrix())), "view");
+        cubeshd.UniformMat4f(m_Camera.GetProjMatrix(), "proj");
 
+        
+        cubeobj.Draw(cubeshd);
         obj.Draw(shd);
-        obj2.Draw(shd);
-        obj3.Draw(shd);
-        obj4.Draw(shd);
-        obj5.Draw(shd_light);
         m_Window.Update();
     }
 }
